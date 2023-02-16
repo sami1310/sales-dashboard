db1155b8ca6850789a64b37d737c2a4f60bbf422
sami1310 <samia.jahin@northsouth.edu>
2023-02-16 13:46:10 +0600

    added line chart

--- dashboard/sales_analytics/views.py
+++ dashboard/sales_analytics/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import SalesData, Category, Region, Month
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -10,17 +11,23 @@ def home_page(request):
 
 
 def dash_board(request):
-    sales_data = SalesData.objects.all()
-    labels = []
-    data = []
-    for entry in sales_data:
-        label = entry.category.category_name + ' - ' + \
-            entry.region.region_name + ' - ' + entry.month.month_name
-        labels.append(label)
-        data.append(entry.sales)
-        context = {
-            'labels': labels,
-            'data': data,
-        }
+
+    # grouping by months in values[...] and aggregating total profit that was grouped by months
+    # example output of this queryset --> {'month__month_name': 'March', 'total_profit': 3500.0}
+    queryset = SalesData.objects.values('month__month_name').annotate(
+        total_profit=Sum('profit'))
+
+    # looping through the dictionary retrived by the 'queryset' and extracting month name
+    # that month name is being added as  labels for x-axis for the graph
+    labels = [data['month__month_name'] for data in queryset]
+
+    # total profit for each month is being added as datas for y-axis for the graph
+    data = [data['total_profit'] for data in queryset]
+
+    # data and labels are being passed as context in the dashboard.html file
+    context = {
+        'labels': labels,
+        'data': data,
+    }
 
     return render(request, 'dashboard.html', context)
